cff08703582e3f0e0c1d6b6a9b6e361a7cb107c8
Milner Chen <milnerchen0@gmail.com>
2023-04-17 12:19:04 -0400

    post schema

---
+++ backend/models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    game_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Game',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Post', postSchema);
