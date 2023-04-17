bbee0917320f7a189c1d7e9f27000a879457470d
Shan Kim <shankim35@gmail.com>
2023-04-17 14:32:08 -0400

    user registration done

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,6 +11,49 @@ router.get('/', function(req, res, next) {
   });
 });
 
+// POST /api/users/register
+router.post('/register', async (req, res, next) => {
+  // Check to make sure no one has already registered with the proposed email or
+  // username.
+  const user = await User.findOne({
+    $or: [{ email: req.body.email }, { username: req.body.username }]
+  });
 
+  if (user) {
+    // Throw a 400 error if the email address and/or email already exists
+    const err = new Error("Validation Error");
+    err.statusCode = 400;
+    const errors = {};
+    if (user.email === req.body.email) {
+      errors.email = "A user has already registered with this email";
+    }
+    if (user.username === req.body.username) {
+      errors.username = "A user has already registered with this username";
+    }
+    err.errors = errors;
+    return next(err);
+  }
+
+  // Otherwise create a new user
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email
+  });
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) throw err;
+    bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+      if (err) throw err;
+      try {
+        newUser.hashedPassword = hashedPassword;
+        const user = await newUser.save();
+        return res.json({ user });
+      }
+      catch(err) {
+        next(err);
+      }
+    })
+  });
+});
 
 module.exports = router;
