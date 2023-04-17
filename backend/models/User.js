32962f26774f8b9137b77bbb76dffeb102888f6e
Samuel Kim <samuelskim420@gmail.com>
2023-04-17 15:35:57 +0000

    complete userSchema

---
+++ backend/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+        username: {
+            type: String,
+            required: true,
+            index: { unique: true }
+        },
+        email: {
+            type: String,
+            required: true,
+            index: { unique: true }
+        },
+        password_digest: {
+            type: String,
+            required: true
+        },
+        session_token: {
+            type: String,
+            required: true,
+            index: { unique: true }
+        },
+        posts: {
+            title: {
+                type: String,
+            required: true
+            },
+            description: {
+                type: String
+            },
+            timestamps: true
+        },
+        comments: [
+            {
+                content: {
+                    type: String,
+                    required: true
+                },
+                timestamps: true
+            }
+        ]
+    }, {
+        timestamps: true
+})
