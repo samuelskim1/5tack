52e7cd1424cb0bcdc9b53c907288d5852ff6c583
Gary <geehor@gmail.com>
2023-04-17 12:14:18 -0400

    Create session actions/reducer and session errors reducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,85 @@
+import jwtFetch from "./jwt";
+
+// ACTION TYPES
+const RECEIVE_CURRENT_USER = 'session/RECEIVE_CURRENT_USER';
+const RECEIVE_SESSION_ERRORS = 'session/RECEIVE_SESSION_ERRORS';
+const CLEAR_SESSION_ERRORS = 'session/CLEAR_SESSION_ERRORS';
+export const RECEIVE_LOGOUT = 'session/RECEIVE_LOGOUT';
+
+
+// ACTIONS
+const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+const logoutUser = () => ({
+  type: RECEIVE_LOGOUT
+});
+
+export const clearSessionErrors = () => ({
+  type: CLEAR_SESSION_ERRORS
+});
+
+
+// THUNK
+export const signup = user => startSession(user, 'api/users/register');
+export const login = user => startSession(user, 'api/users/login');
+
+const startSession = (userInfo, route) => async dispatch => {
+  try {  
+    const res = await jwtFetch(route, {
+      method: "POST",
+      body: JSON.stringify(userInfo)
+    });
+    const { user, token } = await res.json();
+    localStorage.setItem('jwtToken', token);
+    return dispatch(receiveCurrentUser(user));
+  } catch(err) {
+    const res = await err.json();
+    if (res.statusCode === 400) {
+      return dispatch(receiveErrors(res.errors));
+    }
+  }
+};
+
+export const logout = () => dispatch => {
+  localStorage.removeItem('jwtToken');
+  dispatch(logoutUser());
+};
+
+
+// REDUCERS
+export const sessionErrorsReducer = (state = null, action) => {
+  switch (action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+    case CLEAR_SESSION_ERRORS:
+      return null;
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  user: undefined
+};
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return { user: action.currentUser };
+    case RECEIVE_LOGOUT:
+      return initialState;
+    default: 
+      return state;
+  }
+};
+
+export default sessionReducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session';
 
 const rootReducer = combineReducers({
-
+  session
 });
 
 let enhancer;
