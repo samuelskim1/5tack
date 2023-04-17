2466cf4f20ecedc040a89bc3b0195f9f0a1f6a96
Shan Kim <shankim35@gmail.com>
2023-04-17 10:39:30 -0400

    finished express server security

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,13 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
 const usersRouter = require('./routes/api/users');
 const postsRouter = require('./routes/api/posts');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -14,8 +19,29 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, the Express 
+  // server will serve the React files statically.)
+  app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
 // Attach Express routers
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,5 @@
 // backend/config/keys.js
 module.exports = {
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  isProduction: process.env.NODE_ENV === 'production'
 }
