41486a6f83b2465c0736cfd601a24aa3266e5d93
Milner Chen <milnerchen0@gmail.com>
2023-04-18 01:55:35 -0400

    set up edituserinfo form + userinfomodal

--- frontend/src/components/UserInfo/UserInfo.js
+++ frontend/src/components/UserInfo/UserInfo.js
@@ -1,14 +1,20 @@
+import { useRef } from 'react';
 import EditUserInfoForm from '../EditUserInfo/EditUserInfoForm';
+import EditUserInfoModal from '../EditUserInfo/EditUserInfoModal';
 import './UserInfo.scss';
 
 const UserInfo = ({ currentUser }) => {
+  const button = useRef();
+
     return (
         <div className='user-info'>
             <h1>{currentUser.username}</h1>
             <h1>{currentUser.description}</h1>
             <h1>{currentUser.email}</h1>
-            <div>edit profile</div>
-            <EditUserInfoForm currentUser={currentUser} />
+            {/* <EditUserInfoForm currentUser={currentUser} /> */}
+            <div ref={button}>
+                <EditUserInfoModal currentUser={currentUser} />
+            </div>
         </div>
     )
 }
