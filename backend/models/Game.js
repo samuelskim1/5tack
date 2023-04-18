1ce379d0af28aeb9f320f527d3760c9809352592
Shan Kim <shankim35@gmail.com>
2023-04-18 11:19:28 -0400

    finished games routes

--- backend/models/Game.js
+++ backend/models/Game.js
@@ -2,22 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+    name: { 
+      type: String,
+      required: true
     },
     category_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Category',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: 'Game',
+      required: true
     },
-    game_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Game',
-        required: true
-    },
+  }, {
     timestamps: true
-
-});
+  });
 
 module.exports = mongoose.model('Game', gameSchema);
