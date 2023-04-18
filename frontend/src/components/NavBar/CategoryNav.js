044cd72dcc6669f82b2f16ef6efb4a4c7526cad0
Milner Chen <milnerchen0@gmail.com>
2023-04-18 16:23:11 -0400

    create category reducer, create category navbar

---
+++ frontend/src/components/NavBar/CategoryNav.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux"
+import { fetchCategories } from "../../store/categories";
+
+const CategoryNav = () => {
+
+    const dispatch = useDispatch();
+    const categories = useSelector(state => Object.values(state.categories));
+
+    useEffect(() => {
+        dispatch(fetchCategories());
+        console.log(categories);
+    }, []);
+
+    if (!categories) return null;
+
+    return (
+        <ul>{categories.map(cat => <li>{cat.name.split(" ")[0]}</li>)}</ul>
+    )
+}
+
+export default CategoryNav;
