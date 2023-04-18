eba08f20556e0b98e82496469ebef2d76f80f688
Gary <geehor@gmail.com>
2023-04-18 13:48:20 -0400

    Update profile with review/post tabs

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { receiveCurrentUser } from "../../store/session";
 import UserInfo from "../UserInfo/UserInfo";
@@ -7,6 +7,7 @@ import './Profile.scss';
 const Profile = () => {
     // const dispatch = useDispatch();
     const currentUser = useSelector(state => state.session.user);
+    const [tab, setTab] = useState('reviews');
 
     // useEffect(() => {
     //     dispatch( receiveCurrentUser());
@@ -18,6 +19,16 @@ const Profile = () => {
         <div id="profile-container">
             <UserInfo currentUser={currentUser} />
 
+            <div id="review-post-tabs-container">
+                <div id="review-tab">
+                    Reviews
+                </div>
+                <div id="post-tab">
+                    Posts
+                </div>
+            </div>
+
+            
             <div id="reviews-index-container">
                 reviews placeholder
             </div>
