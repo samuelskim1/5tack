0a7028ae1e5662ece98c89dc07019613c303f31e
Gary <geehor@gmail.com>
2023-04-19 09:55:32 -0400

    Work on layout and styling of posts and comments index

--- frontend/src/components/PostIndex/PostIndex.js
+++ frontend/src/components/PostIndex/PostIndex.js
@@ -23,14 +23,14 @@ const PostIndex = ({ posts }) => {
         {/* FOR TESTING PURPOSES */}
         <div className='post-index-item' >
           <div className='post-section'>
+            <div className='post-index-title'>
+              This is a title. This is a title. This is a title. This is a title. This is a title. This is a title. This is a title. 
+            </div>
             <div className='author-info'>
               <Avatar />
               <div className='author-username'>
                 exampleofafairlylongusername
               </div>
-            </div>
-            <div className='post-index-title'>
-              This is a title. This is a title. This is a title. This is a title. This is a title. This is a title. This is a title. 
             </div>
             <div className='post-index-description'>
               this is some actual text like yo wtf is this font hahahaha this is a real text post haha well this is a superlongword text box hello yes haha word hello okay this is some actual text like yo wtf is this font hahahaha this is a real text post haha well this is a superlongword text box hello yes haha word hello okay
