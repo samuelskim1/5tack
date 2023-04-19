7ac70f530acedcc1af0cd2d61299a51bcee31e83
Gary <geehor@gmail.com>
2023-04-18 23:13:13 -0400

    Need to merge main

--- frontend/src/components/PostIndex/PostIndex.js
+++ frontend/src/components/PostIndex/PostIndex.js
@@ -7,7 +7,16 @@ const PostIndex = ({ posts }) => {
 
   return (
     <div className="post-index-container">
-      fjdlsfjdas
+      {posts?.map((post, i) => (
+        <div className='post-index-item' key={i} >
+          <div className='post-index-title'>
+            {post.title}
+          </div>
+          <div className='post-index-description'>
+            {post.description}
+          </div>
+        </div>
+      ))}
     </div>
   )
 };
