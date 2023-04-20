02fd7911efcc510d858dba5aba88aa3cd43fcdd7
Gary <geehor@gmail.com>
2023-04-20 14:26:23 -0400

    Move logout button to bottom of navbar, make navbar links clickable

--- frontend/src/components/CategoryDropdown/CategoryDropdown.js
+++ frontend/src/components/CategoryDropdown/CategoryDropdown.js
@@ -1,12 +1,36 @@
+import { useEffect, useRef } from 'react';
 import './CategoryDropdown.scss';
+import { Link } from 'react-router-dom';
 
-const CategoryDropdown = ({ category }) => {
+const CategoryDropdown = ({ category, toggleShow, dropdown, show }) => {
+
+
+    
+    useEffect(() => {
+        if (show) return;
+        
+        const handleClick = (e) => {
+            // debugger
+            if (!dropdown.current.contains(e.target)) toggleShow()
+        }
+
+        document.addEventListener('click', handleClick)
+
+        return () => document.removeEventListener('click', handleClick)
+    })
+
     return (
-        <div className="dropdown">
+        <>
             <p className='dropdown-name'>{category.name.split("$")[1] ? category.name.split("$")[1] : category.name.split("$")[0]}</p>
             <div className='line-divider'/>
-            {category.game_id.map(game => <li className="dropdown-item">{game.name}</li>)}
-        </div>
+            {category.game_id.map((game, i) => 
+                <li className="dropdown-item" key={i}>
+                    <Link to={`/games/${game.nameURL}`}>
+                        {game.name} 
+                    </Link>
+                </li>
+            )}
+        </>
     )
 }
 
