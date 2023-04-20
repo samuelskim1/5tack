b003d501a02a44704a3b033582c4a9034ab3caf0
Gary <geehor@gmail.com>
2023-04-20 00:33:52 -0400

    Refactor PostIndex to have PostIndexItems

--- frontend/src/components/PostIndex/PostIndex.js
+++ frontend/src/components/PostIndex/PostIndex.js
@@ -1,9 +1,5 @@
-
-import { Link } from 'react-router-dom';
-import CommentsIndex from '../CommentsIndex/CommentsIndex';
-import TimeStamp from '../TimeStamp/TimeStamp';
-import Avatar from '../UserInfo/Avatar';
 import './PostIndex.scss';
+import PostIndexItem from './PostIndexItem';
 
 
 const PostIndex = ({ posts }) => {
@@ -13,29 +9,7 @@ const PostIndex = ({ posts }) => {
   return (
     <div className="posts-index-container">
       {posts?.map((post, i) => (
-        <div className='post-index-item' key={i} >
-          <div className='post-index-title'>
-            {post?.title}
-          </div>
-          <div className='author-block'>
-            <div className='author-info'>
-              <Link to={`/${post?.author_id.username}`}>
-                <Avatar user={post?.author_id} />
-              </Link>
-              <Link to={`/${post?.author_id.username}`}>
-                <div className='author-username'>
-                  {post?.author_id.username}
-                </div>
-              </Link>
-            </div>
-            <TimeStamp post={post} />
-          </div>
-          <div className='post-index-description'>
-            {post?.description}
-          </div>
-          <div className='post-comment-separator' />
-          <CommentsIndex post={post} />
-        </div>
+        <PostIndexItem post={post} key={i} />
       ))}
     </div>
   )
