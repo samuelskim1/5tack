d48f8fb3bffd59e80492b55284a0b7547114e936
Gary <geehor@gmail.com>
2023-04-20 14:32:06 -0400

    Give up on fixing navbar

--- frontend/src/components/CategoryDropdown/CategoryDropdown.js
+++ frontend/src/components/CategoryDropdown/CategoryDropdown.js
@@ -10,25 +10,24 @@ const CategoryDropdown = ({ category, toggleShow, dropdown, show }) => {
         if (show) return;
         
         const handleClick = (e) => {
-            // debugger
             if (!dropdown.current.contains(e.target)) toggleShow()
         }
 
         document.addEventListener('click', handleClick)
 
         return () => document.removeEventListener('click', handleClick)
-    })
+    }, [show])
 
     return (
         <>
             <p className='dropdown-name'>{category.name.split("$")[1] ? category.name.split("$")[1] : category.name.split("$")[0]}</p>
             <div className='line-divider'/>
             {category.game_id.map((game, i) => 
-                <li className="dropdown-item" key={i}>
+                <div className="dropdown-item" key={i}>
                     <Link to={`/games/${game.nameURL}`}>
                         {game.name} 
                     </Link>
-                </li>
+                </div>
             )}
         </>
     )
