a20c9ef99104d3e08523fc65fac6ba122b4414e2
Milner Chen <milnerchen0@gmail.com>
2023-04-20 09:43:51 -0400

    add animation to splash boxes

--- frontend/src/components/SplashBox/SplashBox.js
+++ frontend/src/components/SplashBox/SplashBox.js
@@ -1,8 +1,17 @@
+import { useEffect, useRef } from 'react';
 import './SplashBox.scss';
 
 const SplashBox = ({ title, text }) => {
+    const box = useRef();
+
+    useEffect(() => {
+        setTimeout(() => {
+            box.current.classList.add('fade-in');
+        }, 0)
+    }, [])
+
     return (
-        <div className="splash-box">
+        <div className="splash-box" ref={box}>
             <p className="box-title">{title}</p>
             <p className="box-text">{text}</p>
         </div>
