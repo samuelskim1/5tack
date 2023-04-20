85356e6637f62f8d28a325bc8c7c8d467e4ee258
Milner Chen <milnerchen0@gmail.com>
2023-04-19 23:13:47 -0400

    make carousel

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,7 +1,35 @@
-
+import { register } from 'swiper/element/bundle';
+import './HomePage.scss';
+register();
 
 const HomePage = () => {
 
+    return (
+        <div className="home-container">
+            <div className="header-text">
+                these are some words that.
+            </div>
+            <div className='slider-holder'>
+            <swiper-container navigation="true" loop="true" >
+                <swiper-slide id="home-slide">
+                    <img className='swiper-image' src={"https://5tack.s3.amazonaws.com/public/league_of_legends.jpeg"} />
+                </swiper-slide>
+                <swiper-slide id="home-slide">
+                    <img className='swiper-image' src={"https://5tack.s3.amazonaws.com/public/valorant.webp"} />
+                </swiper-slide>
+                <swiper-slide id="home-slide">
+                    <img className='swiper-image' src={"https://5tack.s3.amazonaws.com/public/maplestory.jpeg"} />
+                </swiper-slide>
+                <swiper-slide id="home-slide">
+                    <img className='swiper-image' src={"https://5tack.s3.amazonaws.com/public/minecraft.jpeg"} />
+                </swiper-slide>
+                <swiper-slide id="home-slide">
+                    <img className='swiper-image' src={"https://5tack.s3.amazonaws.com/public/fifa23.jpeg"} />
+                </swiper-slide>
+            </swiper-container>
+            </div>
+        </div>
+    )
 };
 
 export default HomePage;
