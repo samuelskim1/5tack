7db0d029609fc9ea183d095216970ebdf544845c
Gary <geehor@gmail.com>
2023-04-21 04:18:40 -0400

    Style home page

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -31,25 +31,8 @@ const HomePage = () => {
                     </swiper-slide>
                 </swiper-container>
             </div>
-            <div className="header-text">
-                Welcome to Five Stack, the ultimate platform for gamers seeking to form the perfect team! Our mission 
-                is to bring gamers together by offering a unique and interactive space where you can customize your 
-                profile, connect with like-minded players, and create your dream team.
-            </div>
             <div className="body-text">
-                We understand the importance of compatibility and shared interests when it comes to gaming. That's why 
-                we've made it easy for you to express your gaming preferences and playstyles, allowing you to find the 
-                perfect teammates who share your passion for gaming. With Five Stack, you can create your own team from 
-                a pool of players who have similar interests and gaming styles. No more random pairings or struggling to 
-                find the right partners - we provide the tools you need to build a winning team. Moreover, we believe that 
-                transparency and trust are essential in any team-based environment. To ensure a positive gaming experience 
-                for everyone, we encourage our users to review their teammates after each session. By providing constructive 
-                feedback, you not only help your teammates improve but also contribute to a stronger and more informed gaming 
-                community.
-            </div>
-            <div className="ending-text">
-                Join Five Stack today and discover the ultimate platform for gamers who want to level up their team-based 
-                gaming experience!
+                Gone are the days of living in fear of randomly assigned teammates! We provide the tools you need to craft the perfect gaming experience. Whether casual or competitive, players across the web are discovering the superiority of enjoyable gameplay through intentional matchmaking. Click on a category to find the games you like and create a post to connect with others!
             </div>
         </div>
     )
