afe6da9bd6c7c390effdae6aca185a69f460f392
Gary <geehor@gmail.com>
2023-05-02 17:55:01 -0400

    Remove STACKS and redirects from LostPage

--- frontend/src/components/LostPage/LostPage.js
+++ frontend/src/components/LostPage/LostPage.js
@@ -1,10 +1,36 @@
+import { Link, Redirect, useHistory } from 'react-router-dom';
 import './LostPage.scss';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 const LostPage = () => {
+  const history = useHistory();
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     history.push('/home');
+  //   }, 5500)
+
+  //   return () => clearTimeout();
+  // }, [])
 
   return (
     <div id="lost-page-container">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmoWuQM4KTIU2lQ0gDTmOehx0g99IyBeT92A&usqp=CAU" alt="" />
+      <div id="lost-text">
+        {"Your requested 5TACK resource was not found :("}
+      </div>
+
+      <div id="lost-redirecting">
+        Check out our awesome {" "}
+        <Link to="/home">
+          home page
+        </Link>
+         {" instead! :)"}
+      </div>
+
+      <div id="redirect-img">
+        <img src="https://venngage-wordpress.s3.amazonaws.com/uploads/2022/09/meme_sad_frog.png" alt="pepe sad" />
+      </div>
     </div>
   )
 };
