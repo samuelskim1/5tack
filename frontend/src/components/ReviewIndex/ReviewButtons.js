8594bffb7b590415be44a2c3d3b692f0ad9f6d4c
Milner Chen <milnerchen0@gmail.com>
2023-05-02 22:48:03 -0400

    add delete confirmation modal to reviews

--- frontend/src/components/ReviewIndex/ReviewButtons.js
+++ frontend/src/components/ReviewIndex/ReviewButtons.js
@@ -1,19 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { destroyReview } from '../../store/reviews'
 import UpdateReviewModal from '../ReviewForms/UpdateReviewModal';
+import { Modal } from '../../context/modal';
+import PostForms from '../PostForms/PostForms.scss';
 
 const ReviewButtons = ({ review }) => {
     const dispatch = useDispatch();
 
+    const [showConfirm, setShowConfirm] = useState(false);
+
+    const handleDelete = () => {
+        dispatch(destroyReview(review._id));
+        setShowConfirm(false);
+    }
+
     return (
         <div className='review-buttons-holder'>
             <UpdateReviewModal review={review} />
-            <div className='delete-review-btn' onClick={() => dispatch(destroyReview(review._id))}>
+            <div className='delete-review-btn' onClick={() => setShowConfirm(true)}>
                 <i class="fa-solid fa-trash"></i>
             </div>
+            {showConfirm && (
+                <Modal onClose={() => setShowConfirm(false)}>
+                    <div className='create-post-container create-post-form'>
+                        <h2>Do you want to delete this review?</h2>
+                        <div className='delete-btns'>
+                            <div className='submit-btn' onClick={handleDelete}>Delete</div>
+                            <div className='submit-btn cancel-btn' onClick={() => setShowConfirm(false)}>Cancel</div>
+                        </div>
+                    </div>
+                </Modal>
+            )}
         </div>
     )
 }
 
 export default ReviewButtons;
+
+// dispatch(destroyReview(review._id))
