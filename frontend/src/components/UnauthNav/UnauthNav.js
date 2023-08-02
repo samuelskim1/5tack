ee96be1d248ec173edc075929aeb8e96f912219d
Gary <geehor@gmail.com>
2023-08-02 01:29:59 -0400

    Finish basic layout of Unauth Nav

--- frontend/src/components/UnauthNav/UnauthNav.js
+++ frontend/src/components/UnauthNav/UnauthNav.js
@@ -1,10 +1,36 @@
-
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import './UnauthNav.scss';
 
 const UnauthNav = () => {
 
   return (
-    <nav class="nav-bar" id="unauth-nav">
-      <img src="../../../apple-touch-icon.png"></img>
+    <nav id="unauth-nav">
+      <div id="unauth-content-container">
+        <div id="unauth-title">
+          <Link to="/">
+            <img src="../../../apple-touch-icon.png"></img>
+          </Link>
+
+          <Link to="/">
+            <h1>5TACK</h1>
+            <h2>: where gamers unite against toxicity</h2>
+          </Link>
+        </div>
+
+        <ul id="unauth-buttons-holder">
+          <li>
+            Log in
+          </li>
+
+          <li>
+            Sign up
+          </li>
+
+          <li>
+            About us
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
