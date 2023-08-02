43f1bbb7e41df04ba41a268865d5d1fab017466c
Gary <geehor@gmail.com>
2023-08-02 02:00:00 -0400

    Create Splash Page splash card basic layout

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -1,11 +1,34 @@
 import UnauthNav from "../UnauthNav/UnauthNav";
-
+import './SplashPage.scss'
 
 const SplashPage = () => {
 
   return (
-    <div>
+    <div id="splash-container">
       <UnauthNav />
+
+      <div id="splash-cards-container">
+        <section className="splash-card posts-card">
+          <div className="splash-card-content">
+            <h3>Connect with others to create the most stacked team!</h3>
+
+            <p>
+              Craft the ultimate gaming experience by creating posts to find teammates with similar interests and gaming style.
+              Feeling shy? No problem! Browse through posts made by other users and leave comments.
+            </p>
+          </div>
+        </section>
+
+        <section className="splash-card reviews-card">
+          <div className="splash-card-content">
+            <h3>Promote accountability within gaming communities!</h3>
+
+            <p>
+              Are ur friends toxic??//? u caam eot the rihgt place xd
+            </p>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
