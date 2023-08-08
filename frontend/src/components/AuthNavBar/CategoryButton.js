8bfb3636343b0a372e43f880854ff01a1ddbc714
Milner Chen <milnerchen0@gmail.com>
2023-08-08 02:08:59 -0400

    display games after selecting a category

--- frontend/src/components/AuthNavBar/CategoryButton.js
+++ frontend/src/components/AuthNavBar/CategoryButton.js
@@ -1,73 +1,32 @@
 import { useRef, useState } from "react"
+import { Link } from "react-router-dom";
 // import styled, {keyframes} from 'styled-components';
 
 const CategoryButton = ({category}) => {
 
     const [active, setActive] = useState(false);
+    // const [show, setShow] = useState(false);
     const currBut = useRef();
-    // get location -> pass to style -> use style inline
-    // let xLocation = '500px';
-    // let styles = {'--distLeft': xLocation}
 
     const animate = () => {
         const holder = document.getElementById('category-nav').getBoundingClientRect().left;
-        console.log('hash', currBut.current.getBoundingClientRect());
-        // xLocation = `-${currBut.current.getBoundingClientRect().left}px`;
+        // console.log('hash', currBut.current.getBoundingClientRect());
 
 
 
         const xLocation = holder - currBut.current.getBoundingClientRect().left;
         console.log('this is where the element is located', xLocation);
         const test = document.getElementsByClassName('category-button');
-        // test[0].classList.remove('category-button')
 
-
-        // styles = {'--distLeft': xLocation}
-
-        // console.log('currBUt', currBut);
-        // currBut.current.style=`position:absolute; left:${xLocation}px;`;
-        // currBut.current.animate([
-        //     // {transform: `translateX(-${calc(xLocation)}px) 1s`}
-        //     {transform: `translateX(-${calc(xLocation)}px) 1s`}
-        // ])
-
-        // // adds animation to currBut
-        // currBut.current.animation = "moveLeft 1s";
         currBut.current.style.cssText = `--distLeft: ${xLocation}px;`;
-
-        // styled components thing
-        // const moveLeft = (dist) => keyframes`
-        //     100% { transform: translateX(${dist}px) }
-        // `;
-
         
         setActive(!active)
 
-        // console.log('idsoinwohigengoigjwiognorign', currBut.current.style)
-
-        // currBut.current.animation = `${xLocation => moveLeft(xLocation)} 1s`
-
         for (let but of test) {
-            if (but.innerText != currBut.current.innerText) but.classList.add('inactive');
+            if (but.innerText !== currBut.current.innerText) but.classList.add('inactive');
         }
-        console.log(test);
-        console.log(test[9].classList);
-
-
-        // setTimeout(() => {
-        //     const otherButs = document.getElementsByClassName('inactive');
-        //     for (let but of otherButs) {
-        //         but.style.position = "absolute";
-        //         // but.style.opacity = "0";
-        //     }
-        // }, 500)
-
-        // setTimeout(() => {
-        //     const otherButs = document.getElementsByClassName('inactive');
-        //     for (let but of otherButs) {
-        //         but.style.display = "none";
-        //     }
-        // }, 700)
+        // console.log(test);
+        // console.log(test[9].classList);
 
         const otherButs = document.getElementsByClassName('inactive');
         for (let but of otherButs) {
@@ -80,13 +39,38 @@ const CategoryButton = ({category}) => {
             })
         }
 
-        // getElementById -> active-category button -> get the x-value of this and move to this
+        const games = document.getElementsByClassName('game-button');
+        currBut.current.addEventListener('animationend', () => {
+            for (let game of games) {
+                game.style.display = "block";
+            }
+        })
+
+        // fetch the elements in here?
+        // iterate through them to create and append new elements
+        // remember that they're links
+        // category.game_id.map((game, i) =>
+        //     <Link key={i}
+        //         to={`/games/${game.nameURL}`}
+        //     >
+        //     </Link>
+        // )
     }
 
     return (
-        <div onClick={e => animate(e)} ref={currBut} className={`category-button ${active ? 'active-category-button' : ''}`} >
+        <>
+        <div onClick={e => animate(e)} ref={currBut} className={`style-button category-button ${active ? 'active-category-button' : ''}`} >
             <p>{category.name.split('$')[0]}</p>
         </div>
+        {active && category.game_id.map((game, i) =>
+            <Link className='style-button game-button' key={i}
+                to={`/games/${game.nameURL}`}
+                >
+                    {game.name}
+            </Link>
+        )}
+        </>
+        
     )
 }
 
