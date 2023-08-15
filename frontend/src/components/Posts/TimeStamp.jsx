134141e117c36e9ab500c0f30ca6792c97943873
Gary <geehor@gmail.com>
2023-08-15 04:21:06 -0400

    Change minor CSS style and timestamp to either singular or plural time units

--- frontend/src/components/Posts/TimeStamp.jsx
+++ frontend/src/components/Posts/TimeStamp.jsx
@@ -17,27 +17,47 @@ const TimeStamp = ({ post, review, comment }) => {
 
     let elapsed = secs / 31536000;
     if (elapsed > 1) {
-      return Math.floor(elapsed) + " year(s) ago";
+      if (Math.floor(elapsed) === 1) {
+        return Math.floor(elapsed) + " year ago";
+      } else {
+        return Math.floor(elapsed) + " years ago";
+      }
     }
 
     elapsed = secs / 2592000;
     if (elapsed > 1) {
-      return Math.floor(elapsed) + " month(s) ago";
+      if (Math.floor(elapsed) === 1) {
+        return Math.floor(elapsed) + " month ago";
+      } else {
+        return Math.floor(elapsed) + " months ago";
+      }
     }
 
     elapsed = secs / 86400;
     if (elapsed > 1) {
-      return Math.floor(elapsed) + " day(s) ago";
+      if (Math.floor(elapsed) === 1) {
+        return Math.floor(elapsed) + " day ago";
+      } else {
+        return Math.floor(elapsed) + " days ago";
+      }
     }
 
     elapsed = secs / 3600;
     if (elapsed > 1) {
-      return Math.floor(elapsed) + " hour(s) ago";
+      if (Math.floor(elapsed) === 1) {
+        return Math.floor(elapsed) + " hour ago";
+      } else {
+        return Math.floor(elapsed) + " hours ago";
+      }
     }
 
     elapsed = secs / 60;
     if (elapsed > 1) {
-      return Math.floor(elapsed) + " minute(s) ago";
+      if (Math.floor(elapsed) === 1) {
+        return Math.floor(elapsed) + " minute ago";
+      } else {
+        return Math.floor(elapsed) + " minutes ago";
+      }
     }
 
     return Math.floor(secs) + " second(s) ago";
