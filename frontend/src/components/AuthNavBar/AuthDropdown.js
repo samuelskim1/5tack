a8eb5be57069cc174af06409b21f4848e54ddd8f
Milner Chen <milnerchen0@gmail.com>
2023-08-18 22:50:40 -0400

    change favorites into a dropdown

--- frontend/src/components/AuthNavBar/AuthDropdown.js
+++ frontend/src/components/AuthNavBar/AuthDropdown.js
@@ -7,6 +7,7 @@ import './AuthDropdown.scss';
 const AuthDropdown = () => {
     const currentUser = useSelector(state => state.session?.user);
     const [showDrop, setShowDrop] = useState(false);
+    const [showFav, setShowFav] = useState(false);
     const dropdown = useRef();
     const dispatch = useDispatch();
     const history = useHistory();
@@ -15,7 +16,9 @@ const AuthDropdown = () => {
         const closeDrop = (e) => {
             // dropdown is open and dropdown is on page and the click is not inside the dropdown
             if (showDrop && dropdown.current && !dropdown.current.contains(e.target)) {
+                console.log('when the fuck is this happening')
                 setShowDrop(false);
+                setShowFav(false);
             }
         }
 
@@ -39,23 +42,29 @@ const AuthDropdown = () => {
                 <img src={currentUser?.profileImageUrl} />
             </Link>
             <div className="dropdown-container" ref={dropdown}>
-                <i className="fa-solid fa-angle-down" onClick={() => setShowDrop(!showDrop)} ></i>
+                <i className="fa-solid fa-angle-down" onClick={() => {setShowDrop(!showDrop); setShowFav(false)}} ></i>
                 {showDrop && (
                     <div className="dropdown">
                             <p className="drop-title">Welcome back!</p>
                             <p className="drop-link">@{currentUser?.username}</p>
-                        {!!(currentUser?.favorites[0] != '') &&
-                            <>
                             <div className="user-info-divider"></div>
-                            <div className="drop-title">My Favorites</div>
-                            {currentUser?.favorites.map((fav, i) => (
-                                <Link className='drop-link' to={`/games/${fav.split(' ').join('')}`} key={i}>
-                                    {fav}
-                                </Link>
-                                ))}
+                        {!!(currentUser?.favorites?.length && currentUser?.favorites[0] !== '') &&
+                            <>
+                                <div className="drop-link" onClick={() => setShowFav(!showFav)} >
+                                    <p>My Favorites</p>
+                                    <i className="fa-solid fa-angle-down" ></i>
+                                </div>
+                                {showFav && 
+                                <>
+                                    {currentUser?.favorites.map((fav, i) => (
+                                        <Link className='drop-link fav' to={`/games/${fav.split(' ').join('')}`} key={i}>
+                                            {fav}
+                                        </Link>
+                                        ))}
+                                    <div className="user-info-divider"></div>
+                                </>}
                             </>
                         }
-                        <div className="user-info-divider"></div>
                             <Link className='drop-link' to={'/about'}>
                                 About
                             </Link>
