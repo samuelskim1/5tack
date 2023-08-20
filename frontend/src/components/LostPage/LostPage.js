bd928a9b9e5f6e6ae1c8d635579cd7621e4827ad
Milner Chen <milnerchen0@gmail.com>
2023-08-19 21:47:40 -0400

    fix lost page scss affecting other components

--- frontend/src/components/LostPage/LostPage.js
+++ frontend/src/components/LostPage/LostPage.js
@@ -3,14 +3,16 @@ import './LostPage.scss';
 
 const LostPage = () => {
     return (
-        <div id='auth-cards-background'>
-            <div>
-                <div>Your requested 5TACK resource was not found :(</div>
-                <img src="https://venngage-wordpress.s3.amazonaws.com/uploads/2022/09/meme_sad_frog.png" alt="pepe sad" />
-                <p>Check out our awesome{" "}
-                    <Link to='/home'>home page</Link>
-                    {" "}instead :)
-                </p>
+        <div id='lost-container'>
+            <div id='auth-cards-background'>
+                <div>
+                    <div>Your requested 5TACK resource was not found :(</div>
+                    <img src="https://venngage-wordpress.s3.amazonaws.com/uploads/2022/09/meme_sad_frog.png" alt="pepe sad" />
+                    <p>Check out our awesome{" "}
+                        <Link to='/home'>home page</Link>
+                        {" "}instead :)
+                    </p>
+                </div>
             </div>
         </div>
     )
