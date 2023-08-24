32103c427cc8fdb3fa11e738b13202f0be29cdd7
Milner Chen <milnerchen0@gmail.com>
2023-08-24 02:26:09 -0400

    about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,11 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCurrentUser } from './store/session';
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
-// import NavBar from './components/NavBar/NavBar';
 import SplashPage from './components/SplashPage/SplashPage';
 // import HomePage from './components/HomePage/HomePage';
 import Profile from './components/Profile/Profile';
 // import GameShow from './components/GameShow/GameShow'; 
-// import AboutPage from './components/AboutPage/AboutPage'; 
+import AboutPage from './components/AboutPage/AboutPage'; 
 import LostPage from './components/LostPage/LostPage';
 import AuthNavBar from './components/AuthNavBar/AuthNavBar';
 
@@ -28,7 +27,7 @@ const App = () => {
       <Switch>
         <AuthRoute exact path="/" component={SplashPage} />
         {/* <ProtectedRoute exact path="/home" component={HomePage} /> */}
-        {/* <Route exact path="/about" component={AboutPage} /> */}
+        <Route exact path="/about" component={AboutPage} />
         {/* <ProtectedRoute exact path="/games/:nameURL" component={GameShow} /> */}
         <ProtectedRoute exact path="/:username" component={Profile} />
         {/* <ProtectedRoute exact path="/uh-oh/404" component={LostPage} /> */}
