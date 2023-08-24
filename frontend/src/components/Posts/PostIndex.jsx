28b2cd9d2e6d29987dfceb2f6287ba15d8d1ff95
Gary <geehor@gmail.com>
2023-08-23 23:48:37 -0400

    Fix Game Show Post Index shadow box to adjust height according to window scroll Y

--- frontend/src/components/Posts/PostIndex.jsx
+++ frontend/src/components/Posts/PostIndex.jsx
@@ -1,8 +1,27 @@
 import PostIndexItem from "./PostIndexItem";
 import './PostIndex.scss';
+import { useEffect } from "react";
+import { useRef } from "react";
 
 
 const PostIndex = ({ posts, type }) => {
+  const shadow = useRef();
+  
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY; // => scroll position
+    if (shadow && scrollPosition >= 130 && scrollPosition <= 170) {
+      shadow.current.style.height = scrollPosition - 80 + "px";
+    }
+  };
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  
   if (!posts) {
     return (
       <div>
@@ -10,12 +29,12 @@ const PostIndex = ({ posts, type }) => {
       </div>
     )
   }
-console.log(type);
+
   return (
     <div className="posts-index-container">
       {!type && (
         <div id="game-show-shadow-anchor">
-          <div id="game-show-shadow" />
+          <div id="game-show-shadow" ref={shadow} />
         </div>
       )}
       {posts.map((post, idx) => (
