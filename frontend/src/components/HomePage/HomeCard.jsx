eb1bdcf2eb28e6ca779e32874e51d4e0cead98dc
Gary <geehor@gmail.com>
2023-08-29 22:54:24 -0400

    Alter Auth Cards to prevent false perception of left card scrollability

--- frontend/src/components/HomePage/HomeCard.jsx
+++ frontend/src/components/HomePage/HomeCard.jsx
@@ -6,18 +6,20 @@ const HomeCard = () => {
     <div id="home-card">
       <h2>5TACK</h2>
 
-      <p>
-        Begin creating your dream team by exploring our different featured games! 
-        Whether you are a professional gamer or casually exploring, we offer a variety of different platforms for you to explore your next virtual adventure. 
-      </p>
+      <div>
+        <p>
+          Begin creating your dream team by exploring our different featured games! 
+          Whether you are a professional gamer or casually exploring, we offer a variety of different platforms for you to explore your next virtual adventure. 
+        </p>
 
-      <img src="https://cdn.autonomous.ai/static/upload/images/new_post/nice-and-cozy-gaming-setup-tips-4558-1652713505318.webp" alt="" />
+        {/* <img src="https://cdn.autonomous.ai/static/upload/images/new_post/nice-and-cozy-gaming-setup-tips-4558-1652713505318.webp" alt="" /> */}
 
-      <p>
-        If you are unfamiliar with all the different gaming categories or are simply looking for a new genre to explore, check out the brief explanations on your right. 
-      </p>
+        <p>
+          If you are unfamiliar with all the different gaming categories or are simply looking for a new genre to explore, check out the brief explanations on your right. 
+        </p>
 
-      <i className="fa-regular fa-circle-right" />
+        {/* <i className="fa-solid fa-angles-right" /> */}
+      </div>
     </div>
   )
 };
