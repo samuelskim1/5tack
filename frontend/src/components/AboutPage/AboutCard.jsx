d465b3c5d04c50f3712e452554e94a76895bb116
Gary Hor <geehor@gmail.com>
2024-05-09 00:08:29 -0400

    Modify email link in AboutCard component to send email to actual address rather than abc@example.com

--- frontend/src/components/AboutPage/AboutCard.jsx
+++ frontend/src/components/AboutPage/AboutCard.jsx
@@ -1,23 +1,22 @@
-const AboutCard = ({info}) => {
-
-    return (
-        <div className="about-card">
-            <img src={info?.image} />
-            <div>
-                <p>{info?.name}</p>
-                <p>{info?.role}</p>
-                <a href = "mailto: abc@example.com">{info?.email}</a>
-                <div>
-                    <a href={info?.github} target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-github"></i>
-                    </a>
-                    <a href={info?.linkedin} target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-linkedin"></i>
-                    </a>
-                </div>
-            </div>
+const AboutCard = ({ info }) => {
+  return (
+    <div className="about-card">
+      <img src={info?.image} alt={`${info?.name}!`} />
+      <div>
+        <p>{info?.name}</p>
+        <p>{info?.role}</p>
+        <a href={`mailto: ${info?.email}`}>{info?.email}</a>
+        <div>
+          <a href={info?.github} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href={info?.linkedin} target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin"></i>
+          </a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default AboutCard;
